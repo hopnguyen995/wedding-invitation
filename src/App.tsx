62a67800367aaa018ca3d176c21c0204d758a9c0
CanhVT <tuancanhnb@gmail.com>
2024-02-15 09:05:12 +0700

    test3

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 // import ConfettiCongratulations from "./components/ConfettiCongratulations";
 import Home from "./components/home/home";
-import MusicPlayer from "./components/music";
+// import MusicPlayer from "./components/music";
 
 function App() {
   return (
     <div>
       <Home />
-      <MusicPlayer />
+      {/* <MusicPlayer /> */}
       {/* <ConfettiCongratulations /> */}
     </div>
   );
--- src/components/home/home.tsx
+++ src/components/home/home.tsx
@@ -1,9 +1,9 @@
 import CountdownTimer from "../countdownTimer";
-import MessengerButton from "../redirectMessenger";
+// import MessengerButton from "../redirectMessenger";
 import CarouselHero from "./carouselHero";
-import Info from "./info";
-import imgGroom from "../../assets/images/groom-section-1.jpeg";
-import imgBride from "../../assets/images/bride-section-1.jpeg";
+// import Info from "./info";
+// import imgGroom from "../../assets/images/groom-section-1.jpeg";
+// import imgBride from "../../assets/images/bride-section-1.jpeg";
 import BestRegards from "./bestRegards";
 
 export interface IHomeProps {}
@@ -13,12 +13,17 @@ export default function Home() {
     <div>
       <CarouselHero />
       <BestRegards />
-      <div className="">
+      {/* <div className="">
         <Info urlImg={imgGroom} groom={true} />
         <Info urlImg={imgBride} groom={false} />
-      </div>
+      </div> */}
       <CountdownTimer />
-      <MessengerButton />
+      <CountdownTimer />
+      <CountdownTimer />
+      <CountdownTimer />
+      <CountdownTimer />
+      <CountdownTimer />
+      {/* <MessengerButton /> */}
     </div>
   );
 }
