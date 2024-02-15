980fbf162af3ba295edb74143baf262a79b44102
CanhVT <tuancanhnb@gmail.com>
2024-02-15 08:50:23 +0700

    test

--- src/components/home/info.tsx
+++ src/components/home/info.tsx
@@ -10,17 +10,17 @@ export interface IInfoProps {
 export default function Info(props: IInfoProps) {
   const { urlImg, groom } = props;
 
-  // useEffect(() => {
-  //   AOS.init();
-  //   // Cài đặt các hiệu ứng AOS tại đây (ví dụ: fade-up)
-  //   AOS.refresh(); // Cập nhật AOS khi component được render lại
-  // }, []);
+  useEffect(() => {
+    AOS.init();
+    // Cài đặt các hiệu ứng AOS tại đây (ví dụ: fade-up)
+    AOS.refresh(); // Cập nhật AOS khi component được render lại
+  }, []);
 
   return (
     <div className="info-groom md:flex lg:w-[60%] lg:mx-auto p-4 bg-[#f7faf9]">
       <div
         className={`img-groom md:w-[50%] ${groom ? "order-1" : "order-2"}`}
-        // data-aos={groom ? "zoom-in-right" : "zoom-in-left"}
+        data-aos={groom ? "zoom-in-right" : "zoom-in-left"}
       >
         <img
           className="w-full object-cover rounded-lg border-[2px] border-solid"
@@ -32,7 +32,7 @@ export default function Info(props: IInfoProps) {
         className={`story md:w-[50%] md:flex-col md:my-auto md:px-3 py-2 ${
           groom ? "order-2" : "order-1"
         }`}
-        // data-aos={groom ? "zoom-in-left" : "zoom-in-right"}
+        data-aos={groom ? "zoom-in-left" : "zoom-in-right"}
       >
         <h2 className="text-[12px] md:text-[16px] font-medium">
           {groom ? "Chú rể: Hoàng Hiệp" : "Cô dâu: Minh Nguyệt"}
