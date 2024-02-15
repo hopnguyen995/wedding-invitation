4fd0fba24cb7c7705cfafc3f635dde2106a0b49c
CanhVT <tuancanhnb@gmail.com>
2024-02-15 11:34:26 +0700

    test btn mess 3

--- src/components/redirectMessenger.tsx
+++ src/components/redirectMessenger.tsx
@@ -1,39 +1,16 @@
-import { isMobile } from "react-device-detect";
-
-// const MessengerButton = () => {
-//   const openMessenger = () => {
-//     const userThreadId = "USER_ID"; // Thay USER_ID bằng ID người dùng thích hợp
-//     const messengerLink = isMobile
-//       ? `fb-messenger://user-thread/${userThreadId}`
-//       : `https://www.messenger.com/t/${userThreadId}`;
-
-//     window.location.href = messengerLink;
-//   };
-
-//   return (
-//     <div>
-//       <button
-//         className="p-[10px] bg-blue-500 text-[#fff] font-medium rounded-lg"
-//         onClick={openMessenger}
-//       >
-//         GỬI LỜI CHÚC
-//       </button>
-//     </div>
-//   );
-// };
-
-// export default MessengerButton;
+// import { isMobile } from "react-device-detect";
 
 const MessengerButton = () => {
-  const userThreadId = "USER_ID"; // Thay USER_ID bằng ID người dùng thích hợp
-  const messengerLink = isMobile
-    ? `fb-messenger://user-thread/${userThreadId}`
-    : `https://www.messenger.com/t/${userThreadId}`;
+  // const userThreadId = "USER_ID"; // Thay USER_ID bằng ID người dùng thích hợp
+  // const messengerLink = isMobile
+  //   ? `fb-messenger://user-thread/${userThreadId}`
+  //   : `https://www.messenger.com/t/${userThreadId}`;
 
   return (
     <div>
       <a
-        href={messengerLink}
+        // href={messengerLink}
+        href={`https://m.me/yourfacebookprofilename`}
         target="_blank"
         rel="noopener noreferrer"
         className="p-[10px] bg-blue-500 text-[#fff] font-medium rounded-lg"
